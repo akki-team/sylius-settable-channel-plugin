f19d91334dbdec92de9cdd9f150484f5d9b5f63c
Quentin Briais <quentin.briais@akki.fr>
2024-05-10 12:00:59 +0200

    Initial commit

---
+++ src/Context/SettableChannelContextInterface.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Akki\SyliusSettableChannelPlugin\Context;
+
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
+
+interface SettableChannelContextInterface extends ChannelContextInterface
+{
+    public function setChannel(ChannelInterface|null $channel): void;
+
+    public function reset(): void;
+}
