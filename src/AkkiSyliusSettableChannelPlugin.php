6767b064b9188ff3164019809b1a59b46d1ed42f
Quentin Briais <quentin.briais@akki.fr>
2024-05-10 13:04:22 +0200

    Add missing file

---
+++ src/AkkiSyliusSettableChannelPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akki\SyliusSettableChannelPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class AkkiSyliusSettableChannelPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
